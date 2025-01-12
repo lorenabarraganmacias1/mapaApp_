011d5961e9845c540bfa9c2982ace8a024353276
jhorjanmartinez <jhorjanmartinez13@hotmail.com>
2025-01-12 18:21:52 -0500

    agregado el loadchildren en routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     SharedModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
